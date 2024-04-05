734cdc7006559e257d658e14aad1c1fb1b31ab52
Joshua Reno <jralphur@gmail.com>
2024-04-05 13:09:46 -0700

    added typing, fixed return

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,26 @@
 import random
 
 dictionary = {}
-def get_dict():
-    return dict
 
-def random_word(length = 5) -> str: 
+def get_dict() -> dict:
+    return dictionary
+
+def random_word(length: int = 5) -> str: 
     return random.choice(dictionary[length])
 
-
-def __init__():
+def match_encode(chosen: str, guess: str) -> list:
+    res = []
+    for (c, g) in zip(chosen, guess):
+        if c == g:
+            res.append("EXACT_MATCH")
+        elif g in chosen:
+            res.append("WRONG_PLACE")
+        else:
+            res.append("NO_MATCH")
+        
+    return res
+    
+def __init__() -> None:
     with open("en_US.dic") as f:
         for word in f:
             word = word.rstrip().split('/')[0].upper()
