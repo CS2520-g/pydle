71892c26ee3305c9da588f78ae96c044b5349ce8
Joshua Reno <jralphur@gmail.com>
2024-04-05 14:17:47 -0700

    handle multiple letters on matching

--- dictionary.py
+++ dictionary.py
@@ -8,13 +8,30 @@ def get_dict() -> dict:
 def random_word(length: int = 5) -> str: 
     return random.choice(dictionary[length])
 
+def char_counts(word: str) -> dict:
+    d = {}
+    for c in word:
+        if c in d:
+            d[c] += 1
+        else:
+            d[c] = 1
+
+    return d
+
 def match_encode(chosen: str, guess: str) -> list:
+    chosen = chosen.upper()
+    guess = guess.upper()
+    chosen_counts = char_counts(chosen)
+
     res = []
     for (c, g) in zip(chosen, guess):
         if c == g:
             res.append("EXACT_MATCH")
-        elif g in chosen:
+        elif g in chosen_counts:
             res.append("WRONG_PLACE")
+            chosen_counts[g] -= 1
+            if chosen_counts[g] == 0:
+                del chosen_counts[g]
         else:
             res.append("NO_MATCH")
         
